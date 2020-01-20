6068bd172165bf76fd5c41c15ae8f137c1e834ba
Jan Andreasen <51907684+jaa2019@users.noreply.github.com>
2020-01-20 09:12:24 +0100

    Initial commit

---
+++ H2-Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H2_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
