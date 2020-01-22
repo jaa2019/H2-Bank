98037446f296069cd29dc487d1e9602360311d08
Jan Andreasen <51907684+jaa2019@users.noreply.github.com>
2020-01-22 10:33:49 +0100

    new project

---
+++ H2-Bank/Utilities/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace H2_Bank.Utilities
+{
+    static class FileLogger
+    {
+        static string fileName = "logs.txt";
+
+        static void WriteToLog(string logmessage)
+        {
+            
+        }
+
+        static string ReadFromLog()
+        {
+            return null;
+        }
+
+    }
+}
